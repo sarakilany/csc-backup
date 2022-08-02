31c68e0ff3fd1492e3a5abc6a0154e0b6ec7db0c
marina ghattas <marinaghattas000@gmail.com>
2022-08-02 20:31:49 +0200

    fix: pakage.json errors

--- src/componets/singleNews/SingleNews.jsx
+++ src/componets/singleNews/SingleNews.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-<<<<<<< HEAD
-import { useParams } from "react-router-dom";
-import { useState } from "react";
 import myImg from "../../assets/images/18.jpg";
-=======
-import { useLocation } from 'react-router-dom'
->>>>>>> develope
+import { useLocation } from "react-router-dom";
 
 export default function SingleNews() {
-
-  const location = useLocation()
-  const { singleNews } = location.state
+  const location = useLocation();
+  const { singleNews } = location.state;
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-md-10 m-auto mb-4">
-            <img className="w-100" src={singleNews.urlToImage} alt="singleNews image" />
+            <img
+              className="w-100"
+              src={singleNews.urlToImage}
+              alt="singleNews image"
+            />
           </div>
           <div className="col-md-10 m-auto my-3">
             <h4>{singleNews.title}</h4>
